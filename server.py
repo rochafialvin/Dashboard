265e188d33b5ceddd1151d898ef74e27d6966afa
rochafialvin <rochafi.dev@gmail.com>
2020-03-26 13:59:09 +0700

    scatter

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import json
 
 app = Flask(__name__)
 
+# Histogram dan Box
 def category_plot(cat_plot = 'histoplot', cat_x = 'sex', cat_y = 'total_bill', estimator = 'count'):
     
     dfTips = sb.load_dataset('tips')
@@ -43,6 +44,33 @@ def category_plot(cat_plot = 'histoplot', cat_x = 'sex', cat_y = 'total_bill', e
 
     return graphJSON
 
+# Scattter
+def scatter_plot(cat_x, cat_y):
+
+    dfTips = sb.load_dataset('tips')
+
+    data = [
+        go.Scatter(
+            x = dfTips[cat_x],
+            y = dfTips[cat_y],
+            mode = 'markers'
+        )
+    ]
+
+    layout = go.Layout(
+        title='Scatter',
+        title_x= 0.5,
+        xaxis = {"title" : cat_x},
+        yaxis = {"title" : cat_y}
+    )
+
+    final = {"data" : data, "layout" : layout}
+
+    graphJSON = json.dumps(final, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON
+
+
 @app.route('/')
 def index():
     plot = category_plot()
@@ -78,11 +106,21 @@ def cat_fn():
 
     return render_template('category.html', plot=plot, focus_plot=cat_plot, focus_x=cat_x, focus_y=cat_y, focus_estimator=estimator )
 
-
 @app.route('/scatt_fn')
 def scatt_fn():
+    # Memilih dari menu dropdown
+    # Keduanya akan bernilai None hanya ketika kita mengunjungi via 'Scatter' menu di navbar
+    cat_x = request.args.get('cat_x')
+    cat_y = request.args.get('cat_y')
+
+    # Jika kita klick menu 'Scatter' pada navbar, keduanya akan bernilai None
+    if cat_x == None and cat_y == None:
+        cat_x = 'total_bill'
+        cat_y = 'tip'
+
+    plot = scatter_plot(cat_x, cat_y)
     
-    return "<h1>Homework</h1>"
+    return render_template('scatter.html', plot=plot)
 
 
 
